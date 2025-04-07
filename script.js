be1e518221aaeae2509cb30040b5b2a382a8ffb1
Jason Gibson <76972956+fingergunsDEV@users.noreply.github.com>
2025-04-06 20:36:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,17 @@ import { setupUIHandlers } from './ui.js';
 import { setupCustomLanguageHandlers } from './language-handlers.js';
 import { handleModeChange } from './mode-handler.js';  
 import { getUIElements } from './ui-elements.js';  
+import { initThemeManager } from './theme-manager.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
   const room = new WebsimSocket();
   const elements = getUIElements();  
+  
   await setupUIHandlers(room);
   await setupCustomLanguageHandlers(room);
+  initThemeManager();
 
   // Default to generator mode on page load
   await setTimeout(() => handleModeChange({target:{value: "generator"}}, elements), 250);
 });
+
